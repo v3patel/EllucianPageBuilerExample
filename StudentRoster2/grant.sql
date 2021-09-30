5853bed0aef3619cc7326374a9dbe8170c716d40
Vipul Patel <vipulp@upenn.edu>
2021-09-30 15:24:42 -0400

    no message

---
+++ StudentRoster2/grant.sql
@@ -0,0 +1,11 @@
+GRANT SELECT ON SFRSTCR TO ban_default_pagebuilder_m;
+GRANT SELECT ON SSBSECT TO ban_default_pagebuilder_m;
+GRANT SELECT ON SCBCRSE TO ban_default_pagebuilder_m;
+GRANT SELECT ON SCBCRSE TO ban_default_pagebuilder_m;
+GRANT SELECT ON SFRSTCR TO ban_default_pagebuilder_m;
+GRANT SELECT ON STVTERM TO ban_default_pagebuilder_m;
+
+
+GRANT EXECUTE ON f_format_name TO ban_default_pagebuilder_m;
+
+GRANT EXECUTE ON f_getspridenid TO ban_default_pagebuilder_m;
